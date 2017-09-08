bd2cbde7b713b6e37dc8c853312cf219ae773cc1
Abhishek Batra <abhishek.batra@wingify.com>
2017-09-08 16:21:36 +0530

    Fixed ff sw

--- ffsw-pushcrew.js
+++ ffsw-pushcrew.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/ffsw-ef67a9e0eecab68f2524f33256a0cdcf');
+importScripts('https://cdn.pushcrewstaging.com/sw/ffsw-ef67a9e0eecab68f2524f33256a0cdcf.js');
