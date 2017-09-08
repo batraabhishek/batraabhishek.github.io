c53a48d8add9418b85d352760ddeb355273fc66a
Abhishek Batra <abhishek.batra@wingify.com>
2017-09-08 13:30:53 +0530

    Switched staging

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/885a060de2928a5e6f8ca4e4b827f0dc.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/ef67a9e0eecab68f2524f33256a0cdcf.js');
