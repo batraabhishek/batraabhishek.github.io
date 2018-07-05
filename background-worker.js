aaaedd6513720bf7b397980194089ccecd1e4043
Abhishek Batra <abhishek.batra@wingify.com>
2018-07-05 19:49:34 +0530

    Updated push files

--- background-worker.js
+++
@@ -1,2 +0,0 @@
-var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/ef67a9e0eecab68f2524f33256a0cdcf.js');
---
+++ pushcrew-sw.js
@@ -0,0 +1,2 @@
+var version = 6;
+importScripts('https://cdn.pushcrewstaging.com/sw/b0c4bc6be204a56149c899efcf169344.js');
