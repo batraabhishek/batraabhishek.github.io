b36b788ad663a645cbd6b57ca6f94c67c6a8ed04
Abhishek Batra <abhishek.batra@wingify.com>
2018-07-28 23:25:35 +0530

    updated

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/4855da2440be31b679bbc65c72c683c0.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/4855da2440be31b679bbc65c72c683c0.js');
